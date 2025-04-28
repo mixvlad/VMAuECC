408af5ef5f3c64c5017d56af0ecfa7c1381d91d6
mixvlad <mixa@xakep.ru>
2025-04-28 14:03:49 +0300

    Refactoring, implemented default templates for file generation

--- backend/YamlGenerator.Core/Services/RequirementService.cs
+++ backend/YamlGenerator.Core/Services/RequirementService.cs
@@ -12,6 +12,48 @@ public class RequirementService : BaseTemplateService
     {
         _standardService = standardService;
     }
+
+    private string LoadI18n(CollectorConfig config)
+    {
+        string result;
+        try
+        {
+            result = LoadAssemblyFile($"YamlGenerator.Core.Data.ControlTypes.{config.OsType}.{config.ControlTypeId}.Requirement.i18n.yaml", config);
+        }
+        catch (FileNotFoundException)
+        {
+            result = LoadAssemblyFile($"YamlGenerator.Core.Data.Templates.{config.OsType}.Requirement.i18n.yaml", config);
+        }
+        return result;
+    }
+
+    private string LoadDataRequirementsParameters(CollectorConfig config)
+    {
+        string result;
+        try
+        {
+            result = LoadAssemblyFile($"YamlGenerator.Core.Data.ControlTypes.{config.OsType}.{config.ControlTypeId}.Requirement.DataRequirementsParameters.yaml", config);
+        }
+        catch (FileNotFoundException)
+        {
+            result = LoadAssemblyFile($"YamlGenerator.Core.Data.Templates.{config.OsType}.Requirement.DataRequirementsParameters.yaml", config);
+        }
+        return result;
+    }
+    
+    private string LoadCcrule(CollectorConfig config)
+    {
+        string result;
+        try
+        {
+            result = LoadAssemblyFile($"YamlGenerator.Core.Data.ControlTypes.{config.OsType}.{config.ControlTypeId}.Requirement.User.Check.ccrule.xml", config);
+        }
+        catch (FileNotFoundException)
+        {
+            result = LoadAssemblyFile($"YamlGenerator.Core.Data.Templates.{config.OsType}.Requirement.User.Check.ccrule.xml", config);
+        }
+        return result;
+    }
     
     public string GenerateRequirementPreview(CollectorConfig config)
     {
@@ -19,16 +61,16 @@ public class RequirementService : BaseTemplateService
         // for preview purposes, not the actual ZIP file
         
         // Load the necessary files
-        string i18nContent = LoadAssemblyFile($"YamlGenerator.Core.Data.ControlTypes.{config.OsType}.{config.ControlTypeId}.Requirement.i18n.yaml", config);
-        string dataRequirementsParametersContent = LoadAssemblyFile($"YamlGenerator.Core.Data.ControlTypes.{config.OsType}.{config.ControlTypeId}.Requirement.DataRequirementsParameters.yaml", config);
-        string ccruleContent = LoadAssemblyFile($"YamlGenerator.Core.Data.ControlTypes.{config.OsType}.{config.ControlTypeId}.Requirement.User.Check.ccrule.xml", config);
+        string i18nContent = LoadI18n(config);
+        string dataRequirementsParametersContent = LoadDataRequirementsParameters(config);
+        string ccruleContent = LoadCcrule(config);
         
         // Create a dictionary with file contents
         var fileContents = new Dictionary<string, string>
         {
             { "i18n.yaml", i18nContent },
             { "DataRequirementsParameters.yaml", dataRequirementsParametersContent },
-            { "User.Check.ccrule.xml", ccruleContent }
+            { $"User.{config.Parameters["AddPropertiesName"]}.ccrule.xml", ccruleContent }
         };
         
         // Serialize to JSON
@@ -41,31 +83,9 @@ public class RequirementService : BaseTemplateService
         {
             using (var archive = new ZipArchive(memoryStream, ZipArchiveMode.Create, true))
             {
-                // Generate standard YAML for inclusion in ZIP
-                //string standardContent = _standardService.GetStandardTemplate();
-                
-                // Load other required files
-                string i18nContent = LoadAssemblyFile($"YamlGenerator.Core.Data.ControlTypes.{config.OsType}.{config.ControlTypeId}.Requirement.i18n.yaml", config);
-                string dataRequirementsParametersContent = LoadAssemblyFile($"YamlGenerator.Core.Data.ControlTypes.{config.OsType}.{config.ControlTypeId}.Requirement.DataRequirementsParameters.yaml", config);
-                string ccruleContent = LoadAssemblyFile($"YamlGenerator.Core.Data.ControlTypes.{config.OsType}.{config.ControlTypeId}.Requirement.User.Check.ccrule.xml", config);
-
-                // Add README.txt to the archive
-                // var readmeEntry = archive.CreateEntry("README.txt");
-                // using (var entryStream = readmeEntry.Open())
-                // using (var streamWriter = new StreamWriter(entryStream))
-                // {
-                //     streamWriter.Write("This is a configuration package for data collection.");
-                // }
-
-                // Add standard YAML file
-                // var standardEntry = archive.CreateEntry("standard.yaml");
-                // using (var entryStream = standardEntry.Open())
-                // using (var streamWriter = new StreamWriter(entryStream))
-                // {
-                //     streamWriter.Write(standardContent);
-                // }
                 
                 // Add ccrule file
+                string ccruleContent = LoadCcrule(config);
                 var ccruleEntry = archive.CreateEntry($"UserRequirements/User.{config.Parameters["AddPropertiesName"]}/User.{config.Parameters["AddPropertiesName"]}.ccrule.xml");
                 using (var entryStream = ccruleEntry.Open())
                 using (var streamWriter = new StreamWriter(entryStream))
@@ -74,6 +94,7 @@ public class RequirementService : BaseTemplateService
                 }
 
                 // Add DataRequirementsParameters file
+                string dataRequirementsParametersContent = LoadDataRequirementsParameters(config);
                 var dataRequirementsParametersEntry = archive.CreateEntry($"UserRequirements/User.{config.Parameters["AddPropertiesName"]}/DataRequirementsParameters.yaml");
                 using (var entryStream = dataRequirementsParametersEntry.Open())
                 using (var streamWriter = new StreamWriter(entryStream))
@@ -82,6 +103,7 @@ public class RequirementService : BaseTemplateService
                 }
 
                 // Add i18n file
+                string i18nContent = LoadI18n(config);
                 var i18nEntry = archive.CreateEntry($"UserRequirements/User.{config.Parameters["AddPropertiesName"]}/i18n.yaml");
                 using (var entryStream = i18nEntry.Open())
                 using (var streamWriter = new StreamWriter(entryStream))
